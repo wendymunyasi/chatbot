4d4c304092d111ba0d0b05259c1b6fac3918ede4
wendy munyasi <wendymunyasi@gmail.com>
2023-08-15 13:32:59 +0300

    Integrate openai

--- chatbotAPP/views.py
+++ chatbotAPP/views.py
@@ -1,5 +1,26 @@
+import openai
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    answer = response.choices[0].text.strip()
+    return answer
+
 def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
