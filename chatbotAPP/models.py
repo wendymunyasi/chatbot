af482f0ec7c3e8bb36eeea15bf05220552befbe3
wendy munyasi <wendymunyasi@gmail.com>
2023-08-16 18:14:11 +0300

    Import User model and register it

--- chatbotAPP/models.py
+++ chatbotAPP/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.user.username}: {self.message}'
